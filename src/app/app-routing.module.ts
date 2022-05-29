6d76ba08cdad49019ee6cd6675236ba162429893
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-05-29 19:56:58 +0500

    recipe home component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './module/home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {
+      path: '',
+      component: HomeComponent
+    }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
